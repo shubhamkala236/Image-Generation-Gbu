14cf611a8ccd12c950cc053cc25a6835d3721f6e
shubham01~ <lawkktd@gmail.com>
2023-06-12 17:11:58 +0530

    first commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+import download from './download.png';
+import logo from './logo.svg';
+import gbu from './gbu.png';
+import preview from './preview.png';
+
+export {
+  download,
+  logo,
+  gbu,
+  preview,
+};
